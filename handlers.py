182a6b85469a65081be2d8eff33313d07face71c
Amit Kumar <y13uc022@lnmiit.ac.in>
2016-06-23 10:57:52 +0530

    Added a HTMLRendering class that append specific html tag to a block of words

--- handlers.py
+++ handlers.py
@@ -17,3 +17,48 @@ class Handler:
 		self.callback('end_',name)
 	def sub(self,name):
 		return lambda match: self.callback('sub_',name,match) or match.group(0)
+
+class HTMLRenderer(Handler):
+        """
+
+        A specific handler used for rendering HTML.
+
+        The method in HTMLRenderer are accessed from superclass Handler's
+        start(),end() and sub() methods. They implement basic markup as used
+        in HTML document.
+
+        """
+
+        def start_document(self):
+                print '<html><head><title>...</title></head><body>'
+        def end_document(self):
+                print '</body></html>'
+        def start_paragraph(self):
+                print '<p>'
+        def end_paragraph(self):
+                print '</p>'
+        def start_heading(self):
+                print '<h2>'
+        def end_heading(self):
+                print '</h2>'
+        def start_list(self):
+                print '<ul>'
+        def end_list(self):
+                print '</ui>'
+        def start_listitem(self):
+                print '<li>'
+        def end_listitem(self):
+                print '</li>'
+        def start_title(self):
+                print '<h1>'
+        def end_title(self);
+                print '</h1>'
+        def sub_emphasis(self,match):
+                return '<em>%s</em>'%match.group(1)
+        def sub_url(self,match):
+                return '<a href="%s">%s</a>'%(match.group(1),match.group(1))
+        def sub_mail(self,match):
+                return '<a href="mailto:%s">%s</a>'%(match.group(1),match.group(1))
+        def feed(self,data):
+                print data
+                
